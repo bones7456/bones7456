7bc26f474e4e18adaf9f9dd2fbd50be42b36b5f8
bones7456 <bones7456@5bb1bed8-4257-8e24-cea6-b4e702f11ffd>
2010-03-21 11:52:47 +0000

    开发中

--- trunk/cName/cName.py
+++ trunk/cName/cName.py
@@ -10,56 +10,101 @@ cat = {
 '字源图片':'zy',
 '字形图片':'zx'
 }
+__Label_style__ = '<span size="xx-large" weight="ultrabold">%s</span>'
 
 class WV(webkit.WebView):
+    '''webkit 浏览器，显示字的信息'''
     def __init__(self, html=''):
         webkit.WebView.__init__(self)
+        self.runpath = os.path.dirname(os.path.abspath(__file__))
+        if not html:
+            html = os.path.join(self.runpath, 'welcome.html')
         self.open(html)
     def open(self, html):
         webkit.WebView.open(self, html)
-
-class MainWindow:
-    def __init__(self):
-        self.window = gtk.Window()
-        self.window.set_default_size(750, 550)
-        self.window.set_title('cName')
-
-        self.vbox = gtk.VBox(False, 0)
-        self.runpath = os.path.dirname(os.path.abspath(__file__))
-        wel = os.path.join(self.runpath, 'welcome.html')
-        self.wv = WV(wel)
-        self.e = gtk.Entry()
-        self.e.connect('activate', self.refresh)
-        self.vbox.pack_start(self.e, False, False)
-        sw = gtk.ScrolledWindow()
-        sw.add(self.wv)
-        self.vbox.pack_start(sw)
-
-        self.window.add(self.vbox)
-        self.window.connect("delete_event", self.on_close)
-        self.window.show_all()
-        
-    def refresh(self, obj):
-        self.show(obj.get_text().decode('utf8')[0])
-        
     def show(self, c):
         from wikimarkup import parse
         f = os.path.join(self.runpath, 'data', '%s.wiki' % c)
         if os.path.exists(f):
             wiki = open(f).read()
             wiki = reg.sub(self._pic, wiki)
-            wiki = reg1.sub('(', wiki)
+            wiki = reg1.sub(' (', wiki)
             wiki = reg2.sub(')', wiki)
             html = parse(wiki, showToc=False)
-            self.wv.load_html_string(html, 'file:///')
+            self.load_html_string(html, 'file:///')
         else:
-            self.wv.open(os.path.join(self.runpath, 'err.html'))
+            self.open(os.path.join(self.runpath, 'err.html'))
         
     def _pic(self, m):
         i1, i2, i3 = m.group(1).split('|')
         f = '_'.join([cat[i1], i2, i3]) + '.gif'
         f = os.path.join(self.runpath, 'pic', f)
-        return '<img src="%s"/>' % f
+        return '<img src="%s"/>' % f    
+
+__WV__ = WV()
+
+class Name(gtk.VBox):
+    '''名字'''
+    def __init__(self):
+        gtk.VBox.__init__(self)
+        self.label = gtk.Label('×')
+        self.label.set_use_markup(True)
+        self.ev = gtk.EventBox()
+        self.ev.add(self.label)
+        self.ev.connect('button-press-event', 
+            lambda o,e:__WV__.show(self.label.get_text().decode('utf8')[0]))
+        self.b = gtk.Button('换字')
+        self.b.connect('clicked', self.choose)
+        self.pack_start(self.ev)
+        self.pack_start(self.b, False)
+    def setn(self, char):
+        self.label.set_label(__Label_style__ % char)
+    def choose(self, o):
+        self.setn('哈')
+        
+class FName(gtk.VBox):
+    '''姓'''
+    def __init__(self):
+        gtk.VBox.__init__(self)
+        self.label = gtk.Label()
+        self.label.set_use_markup(True)
+        self.setn('李')
+        self.ev = gtk.EventBox()
+        self.ev.add(self.label)
+        self.ev.connect('button-press-event', 
+            lambda o,e:__WV__.show(self.label.get_text().decode('utf8')[0]))
+        self.s = gtk.Entry()
+        self.s.connect('activate',
+            lambda o:self.setn(o.get_text().decode('utf8')[0]))
+        self.pack_start(self.ev)
+        self.pack_start(self.s,False)
+    def setn(self, char):
+        self.label.set_label(__Label_style__ % char)
+        
+class MainWindow:
+    def __init__(self):
+        self.window = gtk.Window()
+        self.window.set_default_size(900, 600)
+        self.window.set_title('cName')
+
+        self.hbox = gtk.HBox(False, 0)
+        
+        self.fn = FName()
+        self.n1 = Name()
+        self.n2 = Name()
+        self.hbox.pack_start(self.fn, False, False)
+        self.hbox.pack_start(self.n1, False, False)
+        self.hbox.pack_start(self.n2, False, False)
+        sw = gtk.ScrolledWindow()
+        sw.add(__WV__)
+        self.hbox.pack_start(sw)
+
+        self.window.add(self.hbox)
+        self.window.connect("delete_event", self.on_close)
+        self.window.show_all()
+        
+    def refresh(self, obj):
+        __WV__.show(obj.get_text().decode('utf8')[0])
         
     def on_close(self, *args):
         gtk.main_quit()
